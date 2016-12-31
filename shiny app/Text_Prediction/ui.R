64e0ffd8bba7c1d8f6377571eea39a2a8420f568
JeremyBowyer <JeremyBowyer@hotmail.com>
2016-12-30 23:31:11 -0500

    Update ui.R

--- shiny app/Text_Prediction/ui.R
+++ shiny app/Text_Prediction/ui.R
@@ -2,17 +2,21 @@ library(shiny)
 library(shinythemes)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(theme = shinytheme("darkly"),
-                  
+shinyUI(navbarPage(title = "Text Prediction Application",
+                   theme = shinytheme("darkly"),
+                   fluid = TRUE,
+                   tabPanel("About",
+                            mainPanel(includeHTML("About.HTML"))),
+                   tabPanel("Application",
   div(
       div(
-        # Copy the line below to make a text input box
         textInput("text", label = h1("Text Prediction", style = "color: #778899"), value = "", placeholder = "Type a phrase..."),
         textOutput("Prediction", inline = TRUE),
         style = "display: inline-block;"
       ),
-      style = "background-color:#171b1e;height:175px;text-align:center;"
+      style = "height:175px;text-align:center;"
   ),
+  hr(),
   
   conditionalPanel(condition="output.condition != '0' ",
     div(
@@ -28,4 +32,4 @@ shinyUI(fluidPage(theme = shinytheme("darkly"),
       style = "width: 70%; margin: 0 auto;"
     )
   )
-))
+)))
